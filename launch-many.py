3e48d6fb8f34f069a79a22516f5278e9dd746347
Sudnya Diamos <sudnya@deep2.stanford.edu>
2017-04-03 11:39:25 -0700

    ENH: create new dir per experiment run

--- launch-many.py
+++ launch-many.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import subprocess
+from pathlib import Path
 
 logger = logging.getLogger("Launch-many")
 
@@ -14,42 +15,48 @@ class Launcher:
     def __init__(self, default_config, param, experiment_name):
         """
         :param default_config: the default config file
-        :param param: the parameters to be overwritten in the base config
-                             (these are the hyper parameters we search on)
+        :param param: the parameters to be overridden in the base config
+                     (these are the hyper parameters we search on and output 
+                      path)
         :param experiment_name: name of the experiment
         """
         
         output_dir = default_config.get("io").get("output_save_path")
-        #creates a directory with experiment name inside output dir
         output_dir = os.path.join( output_dir, experiment_name)
-        logger.info("Creating output directory at " + str(output_dir))
-        
-        if os.path.isdir(output_dir):
-            msg = "Non-existent data path: {}".format(output_dir)
-            raise ValueError(msg)
 
-        
         override_config = default_config
         logger.debug("override cfg starts as " + str(override_config))
+        
         for val in param.get("value"):
             logger.debug("Hyper param value: " + str(val))
+            
+            #TODO not tested for more than one hyperparams
             for k, v in override_config.items():
                 self.search_and_replace_dict(v, param.get("name"), val)
-            output_cfg_path = self.get_cfg_path(output_dir, param.get("name"), 
-                    val)
+            
+            output_cfg_path, output_dir_exp = self.get_cfg_path(output_dir, 
+                    param.get("name"), val)
+            override_config['io']['output_save_path'] = output_dir_exp
+            
+            if not os.path.exists(output_dir_exp):
+                logger.debug("Creating " + str(output_dir_exp))
+                path = Path(output_dir_exp)
+                path.mkdir(parents=True)
             
             with open(output_cfg_path, 'w') as fp:
                 json.dump(override_config, fp)
-            logger.debug("This iter cfg at" + str(output_cfg_path))
 
             #call dq TODO
-            p = subprocess.Popen(["dq-submit", "python", "train.py","-c",output_cfg_path], stdout=subprocess.PIPE)
-#p = subprocess.Popen(["python", "train.py","-c",output_cfg_path], stdout=subprocess.PIPE)
-            p.communicate()
+            #p = subprocess.Popen(["dq-submit", "python", "train.py", "-c",/
+            #    output_cfg_path], stdout=subprocess.PIPE)
+            #p.communicate()
 
 
     def get_cfg_path(self, output_dir, param_name, val):
-        return os.path.join(output_dir + "_" + param_name + "_" + str(val) + ".cfg")
+        dr = os.path.join(output_dir , param_name + "_" + str(val))
+        cfg = os.path.join(dr, "override.cfg")
+        logger.debug("dir: " + str(dr) + " cfg: " + str(cfg))
+        return cfg, dr
    
 
     #TODO: refactor later
@@ -57,20 +64,21 @@ class Launcher:
 
         if isinstance(cfg, dict):
             for k, v in cfg.items():
-                #print "searching for ", param, " in ", k
                 
                 # is a dict itself
                 if isinstance(v, dict):
                     return self.search_and_replace_dict(v, param, val)
+                
                 # is not a dict, so search with self
                 else:
+                    # if not a dict, then look in current
                     if k == param:
-                        #print "Found key " , k
                         cfg[k] = val
-                        #print "Found and Updated ", k , " to ", cfg[k]
                         return
         
         return
+
+
 
 def get_default_config():
     retVal = {
@@ -104,7 +112,7 @@ def main():
     is_verbose       = arguments['verbose']
     base_config      = arguments['base_config']
     param_config     = arguments['parameter_config']
-    experiment_name  = arguments['experiment_name']
+    experiment_name  = arguments['experiment_name'].rstrip()
 
     if is_verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -113,7 +121,7 @@ def main():
     
 
     default_config = {}
-    param = {}
+    param          = {}
     if not base_config:
         default_config = get_default_config()
         logger.debug("No base cfg supplied, so using default - " + str(base_config))
@@ -130,8 +138,9 @@ def main():
     if not experiment_name:
         raise ValueError("Mandatory to specify experiment name")
 
-
     launcher = Launcher(default_config, param, experiment_name)
+
+
 
 if __name__ == '__main__':
     main()
