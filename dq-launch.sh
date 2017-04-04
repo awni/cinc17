bcc3d0dc5a520a0a3e8aa7588b1ae04272da83b2
Sudnya Diamos <sudnya@deep2.stanford.edu>
2017-04-04 15:22:00 -0700

    BUG: typo LD_LIBRARY_PATH; append to /deep/group/sudnya/ecg_env/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/afs/cs/software/sbin:/afs/cs/software/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/deep/group/dq:/usr/local/cuda-8.0/bin:

--- dq-launch.sh
+++ dq-launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo This job is running on $(hostname)
-export LD_LIBARY_PATH=/usr/local/cuda-8.0/lib64
-export PATH=/usr/local/cuda-8.0/bin
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+export PATH=$PATH:/usr/local/cuda-8.0/bin
 python train.py -c $DQ_CFG
 echo "Started training at: ", $(date +%m:%d:%y-%H:%M:%S)
