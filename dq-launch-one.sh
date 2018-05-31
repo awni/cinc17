26f1ce1928785b8dbea2742d1891fb714bea57d9
awni <awni.hannun@gmail.com>
2018-05-31 07:28:47 -0700

    remove dq specifics

--- dq-launch-one.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo This job is running on $(hostname)
-export LD_LIBARY_PATH=/usr/local/cuda-8.0/lib64
-export PATH=/usr/local/cuda-8.0/bin
-python train.py -c cnn2-size256_128-num64_64.json
-echo "Started training at: ", $(date +%m:%d:%y-%H:%M:%S)
--- dq-launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo This job is running on $(hostname)
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
-export PATH=$PATH:/usr/local/cuda-8.0/bin
-python train.py -c $DQ_CFG
-echo "Started training at: ", $(date +%m:%d:%y-%H:%M:%S)
